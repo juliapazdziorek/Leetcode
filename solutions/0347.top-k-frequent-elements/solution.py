ec1f54e71a3364cf630ad3e7cf4afeed00847486
Jula <julia.pazdziorek@gmail.com>
2026-07-24 09:54:33 +0200

    refactor: 0347 change solution to bucket sort

--- solutions/0347.top-k-frequent-elements/solution.py
+++ solutions/0347.top-k-frequent-elements/solution.py
@@ -9,9 +9,28 @@ from leetgo_py import *
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = Counter(nums)
-        items = sorted(counter.items(), key=lambda x: x[1], reverse=True)
-        return [number for number, frequency in items[:k]]
+
+        # counter = Counter(nums)
+        # items = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+        # return [number for number, frequency in items[:k]]
+
+        nums_dict = {}
+        for num in nums:
+            nums_dict[num] = nums_dict.get(num, 0) + 1
+
+        buckets = [[] for _ in range(len(nums) + 1)]
+        for key, value in nums_dict.items():
+            buckets[value].append(key)
+
+        result = []
+        for frequency in range(len(buckets) - 1, 0, -1):
+            for num in buckets[frequency]:
+                result.append(num)
+                if len(result) == k:
+                    return result
+
+        return result
+
 
 
 # @lc code=end
